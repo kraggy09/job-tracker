48717488c150baa1b9c021f79223fb76f3e54d88
Kaif Shaikh <kaifshaikh2013.sk@gmail.com>
2024-09-27 18:34:52 +0530

    Working on the navbar

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="min-w-full min-h-[15vh]  shadow-lg bg-white">
+      <h1>CareerScout</h1>
+      <ul>
+        <li>Login</li>
+        <li>Signup</li>
+        <li>Dashboard</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
